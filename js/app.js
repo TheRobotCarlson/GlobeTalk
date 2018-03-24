682cea6d5bb531936329d1780407bed480c16e77
Brian Carlson <briancarlson6174@gmail.com>
2018-03-24 18:15:47 -0400

    basic setup for webapp

---
+++ js/app.js
@@ -0,0 +1,42 @@
+var apiKey = "46086632";
+var sessionId = "1_MX40NjA4NjYzMn5-MTUyMTkxNDk0MTM4OX5EWU03bmFEMnFWSTJ0NVZpNElKbjBaOUx-UH4";
+var token = "33dd435ad2b3e23245adf5ecb8d6fccc6a52a463";
+
+// (optional) add server code here
+initializeSession();
+
+// Handling all of our errors here by alerting them
+function handleError(error) {
+  if (error) {
+    alert(error.message);
+  }
+}
+
+function initializeSession() {
+  var session = OT.initSession(apiKey, sessionId);
+
+  // Subscribe to a newly created stream
+  session.on('streamCreated', function(event) {
+    session.subscribe(event.stream, 'subscriber', {
+      insertMode: 'append',
+      width: '100%',
+      height: '100%'
+    }, handleError);
+  });
+  // Create a publisher
+  var publisher = OT.initPublisher('publisher', {
+    insertMode: 'append',
+    width: '100%',
+    height: '100%'
+  }, handleError);
+
+  // Connect to the session
+  session.connect(token, function(error) {
+    // If the connection is successful, publish to the session
+    if (error) {
+      handleError(error);
+    } else {
+      session.publish(publisher, handleError);
+    }
+  });
+}
